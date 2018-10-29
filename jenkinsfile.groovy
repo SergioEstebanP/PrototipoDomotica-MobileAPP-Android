42a60f394201d6fba661013f1fd92f41f5b6b402
Sergio Esteban <Sergio.Esteban@externals.adidas.com>
2018-10-29 12:02:39 +0100

    configuring first jenkins file in project

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,3 +1,25 @@
-node {
-
+pipeline {
+    agent any
+    stages {
+        stage('Get source code') {
+            steps {
+                echo 'getting source code ...'
+            }
+        }
+        stage('Build') {
+            steps {
+                echo 'building ...'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'testing ...'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'deploying results ...'
+            }
+        }
+    }
 }
